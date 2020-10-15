21b23b980a6080339d46ff9913a084977b2a6e18
Jung taeWoong <60641307+jtwjs@users.noreply.github.com>
2020-10-15 15:08:04 +0900

    Update: JS/Practice/calculator

---
+++ Vanilla JS/Practice/JS/calculator.js
@@ -0,0 +1,120 @@
+(() => {
+const calculator = document.querySelector('.calculator');
+const output = document.querySelector('.output');
+let number = '0';
+let number2;
+let isNumber;
+let operator;
+
+
+function setPlaceValue() {
+    const arrayNumber = number.split('');
+    const reverseNumber = arrayNumber.reverse();
+    let result = "";
+    
+    for(let i = 0; i < reverseNumber.length ; i++) {
+        if(i%3 === 0 && i !== 0) 
+            result += ",";
+        result += reverseNumber[i];
+      
+     }
+     result = result.split('').reverse().join('');
+     return result;
+     
+}
+
+function printNumber() {
+    if(number.length > 3){
+        output.value = setPlaceValue();
+    }else {
+        output.value = number;
+    }
+    
+}
+
+function calculate(operator) {
+    const value = Number(number2);// 앞숫자
+    const value2 = Number(number);// 뒷숫자
+    switch(operator) {
+        case '+': 
+         number = value + value2;
+         break;
+        case '-':
+         number = value - value2;
+         break;
+        case '*':
+            number = value * value2;
+        break;
+        case '/':
+            number = value / value2;
+        break;
+    }
+    number = number +"";
+    number2 = undefined;
+    printNumber();
+}
+
+function clickHandler(e) {
+    const target = e.target;
+    
+    if(!target.classList.contains('button'))
+        return;
+
+    if(target.classList.contains('cancle')) {
+        number = '0';
+        number2 = undefined;
+        printNumber();
+        return;
+    }
+
+   switch(target.dataset.type) {
+       case 'number': {
+           if(number === '0'){
+                number = target.dataset.calc;
+           } else{
+                number += target.dataset.calc;
+           } 
+
+           isNumber = true;
+           printNumber();
+        break;
+       }
+       case 'operator' : {                   
+            if(number2  && number && isNumber){
+                calculate(operator);
+            }
+
+           if(isNumber){  
+               number2 = number;
+               number = '0';
+            }
+
+            operator = target.dataset.calc;
+           isNumber = false;
+           break;
+       }
+   }
+}
+
+function mousedownHandler(e) {
+    const target = e.target;
+    if(!target.classList.contains('button'))
+        return;
+        target.style.opacity = '0.7';
+}
+
+function mouseupHandler(e) {
+    const target = e.target;
+    if(!target.classList.contains('button'))
+        return;
+        target.style.opacity = '1';
+}
+
+function init() {
+    calculator.addEventListener('click', clickHandler);
+    calculator.addEventListener('mousedown', mousedownHandler);
+    calculator.addEventListener('mouseup', mouseupHandler);
+}
+init();
+
+})();
