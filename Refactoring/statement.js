5a9f45a442ddbea24d55c61f8a29b427ad0397b2
Jung taeWoong <60641307+jtwjs@users.noreply.github.com>
2020-11-02 18:40:35 +0900

    Refactoring: create htmlStatement

--- Refactoring/statement.js
+++ Refactoring/statement.js
@@ -1,3 +1,4 @@
+const { result } = require('underscore');
 const currency = require('./createStatementData.js');
 const createStatementData = currency.createStatementData;
 
@@ -52,15 +53,31 @@ function renderPlainText(data, plays) {
 
     result += `총액: ${usd(data.totalAmount)}\n`;
     result += `적립포인트: ${data.totalVolumeCredits}점\n`;
-    return result;
-    
-    function usd(aNumber) {
-        return new Intl.NumberFormat("en-US",
-                            {style: "currency", currency: "USD",
-                        minimumFactionDigits: 2}).format(aNumber/100);
-    }
-
-    
+    return result;    
 }
 
+function htmlStatement(invoice, plays) {
+    return renderHtml(createStatementData(invoice, plays)); //중간 데이터 생성 함수를 공유
+}
+
+function renderHtml(data) {
+    let reulst = `<h1>청구 내역 (고객명: ${data.customer})</h1>\n`;
+    result += "<table>\n";
+    result += "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>";
+    for(let perf of data.performances) {
+        result +=  `<tr><td>${perf.play.name}</td><td>${perf.audience}석)</td>`;
+        result += `<td>${usd(perf.amount)}</td></tr>\n`;
+    }
+    result += "</table>\n";
+    result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+    result += `<p>적립 포인트: <em>${data.totalVolumeCredits}</em>점</p>\n`;
+
+    return result;
+}
+
+function usd(aNumber) {
+    return new Intl.NumberFormat("en-US",
+                        {style: "currency", currency: "USD",
+                    minimumFactionDigits: 2}).format(aNumber/100);
+}
 console.log(statement(invoices,plays));
