9c72ddb7350dfadbddd40a80566fcae456ff56c6
Jung taeWoong <60641307+jtwjs@users.noreply.github.com>
2020-11-06 18:28:38 +0900

    Refactoring: 함수 인라인하기

--- TDD/src/369.js
+++ TDD/src/369.js
@@ -1,36 +1,36 @@
-'use strict';
+// 'use strict';
 
-class NotNumberError extends Error {
-    constructor() {
-      super('숫자가 아닌 값이 포함되어 있습니다.');
-      this.name = 'NotNumberError';
-    }
-  }
+// class NotNumberError extends Error {
+//     constructor() {
+//       super('숫자가 아닌 값이 포함되어 있습니다.');
+//       this.name = 'NotNumberError';
+//     }
+//   }
 
-class NotArrayError extends Error {
-    constructor() {
-      super('배열이 아닙니다.');
-      this.name = 'NotArrayError';
-    }
-  }
+// class NotArrayError extends Error {
+//     constructor() {
+//       super('배열이 아닙니다.');
+//       this.name = 'NotArrayError';
+//     }
+//   }
 
- function game(arr) {
-    if (!Array.isArray(arr)) {
-        throw new NotArrayError();
-      }
+//  function game(arr) {
+//     if (!Array.isArray(arr)) {
+//         throw new NotArrayError();
+//       }
     
-      if (arr.some((num) => typeof num !== 'number')) {
-        throw new NotNumberError();
-      }
+//       if (arr.some((num) => typeof num !== 'number')) {
+//         throw new NotNumberError();
+//       }
 
-     return arr
-        .map( (num) => {
-            const str = String(num).replace(/[^369]/g, '');
-            if(/[369]/.test(str)) {
-                return '짝'.repeat(str.length);
-            }
+//      return arr
+//         .map( (num) => {
+//             const str = String(num).replace(/[^369]/g, '');
+//             if(/[369]/.test(str)) {
+//                 return '짝'.repeat(str.length);
+//             }
 
-         return num;
-     })
-      .join(',');
-}
+//          return num;
+//      })
+//       .join(',');
+// }
